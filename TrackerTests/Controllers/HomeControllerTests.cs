b3dd2a5fd0e1576c1c5197d3d39adb475d48422d
mh37 <78908148+mh37@users.noreply.github.com>
2022-04-15 00:58:32 +0300

    improving the view files and adding test class

---
+++ TrackerTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tracker.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracker.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void HomeControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ErrorTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
+
+namespace TrackerTests.Controllers
+{
+    internal class HomeControllerTests
+    {
+    }
+}
