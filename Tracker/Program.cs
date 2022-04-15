64fbae7f5d51c5a5604a3f98480e7953dfcd55c3
mh37 <78908148+mh37@users.noreply.github.com>
2022-04-15 17:57:05 +0300

    declutter account management interface

--- Tracker/Program.cs
+++ Tracker/Program.cs
@@ -15,6 +15,9 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 builder.Services.AddControllersWithViews();
 
 
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
